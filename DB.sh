a9a9e1187f4f8c717d5b307d9f1843c89a0f1f05
Max <zspmilan@163.com>
2019-07-22 17:38:33 +0800

    change files

--- DB.sh
+++ DB.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /var/lib/jenkins/workspace/jenkis-docker
-docker bulid -t centos-py-nginx .
+imageID=docker bulid .|grep "Successfully built"|awk '{print $3}'
+docker run -p 5579:80 --name centos-mine -d $imageID sleep 1d
